4baa48ebc2223d818a002362578854da2a91d428
Erik Bamberg <erik.bamberg@web.de>
2023-06-18 12:21:59 +0100

    TabularQA example

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # demonstration-vector-database
 some example use cases for vector databases in real world
+
+# Getting Started
+
+## start Weaviate cluster
+
+you need kubernetes running and configured on your system.
+kubectl has to be installed and points to your kubernetes cluster
+
+~~~
+ cd infrastructure
+ kubectk apply -f deployment.yaml
+ kubectl port-forward service/weaviate-service 8080:8080
+~~~
+
+
+## setting up environemt
+
+Please set the connection parameters to your weaviate cluster in your system environemt variables.
+
+| ENV | Optional |EXAMPLE  | Description
+| :---: | :---: | :---: | :--- |
+| "WEAVIATE_URL"| mandatory |"http://localhost:8080"| the url to the weaviate cluste
+|"WEAVIATE_API_KEY" | optional | | The API Key of Weaviate cluster or Empty if authentication is disabled on the cluster. For a cluster running on localhost the auth is alwaus disabled for now.
+|"HUGGINGFACE_APIKEY" | optional | | The API Key of your huggingface account for examples using cloud interference |
+
+
+## examples
+
+~~~
+ cd examples
+~~~
+
+## Tabular QA
+
+Tabular QA demonstrated a chat robot answering question based on CSV-Tables in weaviate database.
+
--- examples/tabularQA.py
+++ examples/tabularQA.py
@@ -5,10 +5,16 @@
 
 import pandas as pd
 import torch
+import os
+import json
+from tqdm import tqdm
+from io import StringIO
+
 # Vector DB imports
 import weaviate
 # Hugging Face Imports
 from sentence_transformers import SentenceTransformer
+from transformers import pipeline, TapasTokenizer, TapasForQuestionAnswering
 from datasets import load_dataset
 
 
@@ -17,26 +23,28 @@ from datasets import load_dataset
 #       Configuration
 #
 
-database="tabularqa-wv3kwpra"
-api_key="ADF6qZZhU0ha8rF989Sw01LLZBP9Y8VEUyx6"
-
+databaseurl=os.getenv("WEAVIATE_URL", "http://localhost:8080")
+api_key=os.getenv("WEAVIATE_API_KEY","")
+huggingface_key=os.getenv("HUGGINGFACE_APIKEY")
+reinitDatabase=False
 
 #
 #       Definitions
-#
+#auth
 
 class VectorDB:
     def __init__(self):
+        auth=weaviate.AuthApiKey(api_key=api_key) if api_key and not "//localhost" in databaseurl else None
         self.client = weaviate.Client(
-            url=f"https://{database}.weaviate.network",  # Replace with your endpoint
-            auth_client_secret=weaviate.AuthApiKey(api_key=api_key),
+            url=databaseurl, 
+            auth_client_secret=auth,
             additional_headers={
-                "X-Cohere-Api-Key": "xxx",
-                "X-HuggingFace-Api-Key": "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",
-                "X-OpenAI-Api-Key": "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",
+                "X-HuggingFace-Api-Key": huggingface_key,
             },
         )
+      #  self.client.batch
 
+vectorDB=VectorDB()
 
 #
 #       Table Data Functions
@@ -44,35 +52,126 @@ class VectorDB:
 # return a List of panda-dataframes each dataframe represents 1 loaded table with data and header 
 def loadTables():
     # load the dataset from huggingface datasets hub
-    data = load_dataset("ashraq/ott-qa-20k", split="train")
+    data = load_dataset("ashraq/ott-qa-20k", split="train[:10000]")
     tables = []
-    for doc in data:
+    print (data[2])
+    for doc in tqdm(data, colour = 'blue'):
         table = pd.DataFrame(doc["data"], columns=doc["header"])
-        tables.append(table)
+        table["table title"]=doc["title"]
+        processed_table = "\n".join([table.to_csv(index=False)])
+        tables.append( { 
+                            "id": doc["uid"],
+                            "name" : doc["title"],
+                            "url": doc["url"],
+                            "location": "",
+                            "table": table,
+                            "tableAsString": processed_table
+                            })
     return tables
 
-def preprocess_tables(tables: list):
-    processed = []
-    for table in tables:
-        processed_table = "\n".join([table.to_csv(index=False)])
-        print (processed_table)
-        processed.append(processed_table)
-    return processed
 
+#
+#       Vector DB Functions
+#
+def insertdata(tables):
+        with vectorDB.client.batch(batch_size=200) as batch:
+             for t in tqdm(tables, colour = 'blue'):
+                  vec=encoder.encode(t["tableAsString"])
+                  properties = {
+                       "name": t["name"],
+                       "url": t["url"],
+                       "document": t["tableAsString"]
+                  }
+
+                  vectorDB.client.batch.add_data_object(
+                       properties, "Tables", vector=vec
+                  )
+
+def createschema(className):
+
+    if vectorDB.client.schema.exists(className):
+        vectorDB.client.schema.delete_class(className)
+
+    test_schema = {
+        "class" :  className,
+        "properties": [
+             {"name":"name", "dataType":["text"]},
+             {"name":"url", "dataType":["text"]},
+             {"name":"document", "dataType":["text"]}
+
+        ]         
+    }
+
+    vectorDB.client.schema.create_class(test_schema)
+
+def initDatabase():
+    createschema("Tables")
+    tables=loadTables()
+    print (tables[2]["table"])
+
+    insertdata(tables)
 #
 #   Encoding
 #
-def getEncoder():
+def buildEncoder():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     return SentenceTransformer("deepset/all-mpnet-base-v2-table", device=device)
 
+def loadQAPipeline():
+     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+     model_name = "google/tapas-base-finetuned-wtq"
+     tokenizer = TapasTokenizer.from_pretrained(model_name)
+     model = TapasForQuestionAnswering.from_pretrained(model_name, local_files_only=False)
+     return pipeline("table-question-answering", model=model, tokenizer=tokenizer, device=device)
+
+def executeQuery(query):
+    result = (
+         vectorDB.client.query
+            .get("Tables",["name","url","document"])
+            .with_near_vector ( {"vector": encoder.encode(query)  } )
+            .with_limit(1)
+            .do()
+    )
+
+    resTable=result["data"]["Get"]["Tables"][0]
+    resCSV=resTable["document"]
+    resName=resTable["name"]
+    resDataFrame=pd.read_csv(StringIO(resCSV))
+    
+    response=qapipeline(table=resDataFrame, query=query)
+    print(json.dumps(response, indent=4))
+    answer=response["answer"]
+    coordinates=response["coordinates"]
+    resDataFrame.style.applymap(highlight_cell) # axis=1, subset=[coordinates[0][1]])
+    print (resName,"\n",resDataFrame)
+    print ("\n\nthe answer is:",answer)
+
+#
+#   output formating
+#
+
+
+def highlight_cell(row):
+    highlight = 'background-color: palegreen;'
+    print("highlighter")
+    return highlight
 
 #
 #    -------------  MAIN --------------
 #
 
-tables=preprocess_tables(loadTables())
-print (tables[2])
+if __name__ == '__main__':
+    encoder=buildEncoder()
+    qapipeline=loadQAPipeline()
 
-encoder=getEncoder()
+    if reinitDatabase:
+        initDatabase()
+
+    query = "who is the manager of the Jackson Mets"
+    executeQuery(query)
+
+    query = "which teams has the New York Mets"
+    executeQuery(query)
+
+
 
