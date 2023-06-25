863408dbed11ab954c23fb8756a7c3328f9d1a36
Erik Bamberg <erik.bamberg@web.de>
2023-06-25 20:21:31 +0100

    introduced metrics module

---
+++ examples/common/metrics/__init__.py
@@ -0,0 +1,11 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import confusion_matrix
+
+#
+#       Definitions
+#
+
+
+class ConfusiMatrix:
+    def __init__(self):
+        pass
