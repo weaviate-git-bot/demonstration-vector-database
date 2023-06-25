58b1f51299b12b8d77453e8cd1e095546fe17474
Erik Bamberg <erik.bamberg@web.de>
2023-06-25 20:25:50 +0100

    introduced metrics module

--- examples/common/metrics/__init__.py
+++ examples/common/metrics/__init__.py
@@ -9,3 +9,18 @@ from sklearn.metrics import confusion_matrix
 class ConfusiMatrix:
     def __init__(self):
         pass
+
+    def show(self):
+        # Create confusion matrix
+        conf_matrix = confusion_matrix(y_true, y_pred)
+
+        # Show confusion matrix
+        ax = plt.subplot()
+        sns.heatmap(conf_matrix, annot=True, ax = ax, cmap='Blues', fmt='g', cbar=False)
+
+        # Add labels, title and ticks
+        ax.set_xlabel('Predicted')
+        ax.set_ylabel('Acctual')
+        ax.set_title('Confusion Matrix')
+        ax.xaxis.set_ticklabels(['Benign', 'Attack'])
+        ax.yaxis.set_ticklabels(['Benign', 'Attack'])
